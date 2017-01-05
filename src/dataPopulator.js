e1f9686a4eb439970d7b603df8af33526f3e6c06
Jason Menayan <jasonmenayan@gmail.com>
2017-01-05 15:41:41 -0800

    working dataPopulator; 3 most recent years

--- src/dataPopulator.js
+++ src/dataPopulator.js
@@ -2,45 +2,45 @@
 
 const fs = require('fs')
 
-// names = {someName: [{year: 1990, gender: 'F', score: 299}, {year: 1991, gender: 'F', score: 283}], otherName: []}
-// years = {'1990': {someName: [{gender: 'F', score: 299}], otherName: [{gender: 'M', score:301}]}}
-
 const names = {}
 const years = {}
 
-fs.readdir('./namesData', (err, files) => {
-	files.forEach(file => {
-		var year = file.substr(3, 4)
-		var yearObj = {}
-		fs.readFile(`./namesData/${file}`, 'utf8', (err, data) => {
-			console.log('in readFile')
-			if (err) throw new Error(err)
-			var arr = data.split('\n')
-			arr.forEach(record => {
-				var recordArray = record.trim().split(',')
-				var name = recordArray[0]
-				var gender = recordArray[1]
-				var score = recordArray[2]
-				// populate 'names' object
-				var nameObj = {year: year, gender: gender, score: score}
-				if (names.hasOwnProperty(name)) {
-					names[name].push(nameObj)
-				} else {
-					names[name] = [nameObj]
-				}
-				// populate 'years' object & add to yearObj and then years object
-				var yearNameObj = {gender: gender, score: score}
-				if (yearObj.hasOwnProperty(name)) {
-					yearObj[name].push(yearNameObj)
-				} else {
-					yearObj[name] = [yearNameObj]
-				}
-			})
-		})
-		console.log('outside readFile')
-		years[year] = yearObj
+console.time('populate data')
+var files = fs.readdirSync('./namesData')
+files.forEach(file => {
+	var year = file.substr(3, 4)
+	var yearObj = {}
+	var fileData = fs.readFileSync(`./namesData/${file}`, 'utf8')
+	var fileArr = fileData.split('\n')
+	fileArr.forEach(record => {
+		var recordArray = record.trim().split(',')
+		var score = recordArray[2]
+		if (Number(score) > 100) {
+			var name = recordArray[0]
+			var gender = recordArray[1]
+			// populate 'names' object
+			var nameObj = {year: year, gender: gender, score: score}
+			if (names.hasOwnProperty(name)) {
+				names[name].push(nameObj)
+			} else {
+				names[name] = [nameObj]
+			}
+			// populate 'years' object & add to yearObj and then years object
+			var yearNameObj = {gender: gender, score: score}
+			if (yearObj.hasOwnProperty(name)) {
+				yearObj[name].push(yearNameObj)
+			} else {
+				yearObj[name] = [yearNameObj]
+			}
+		}
 	})
-	console.log(names)
-	console.log(years)
+	years[year] = yearObj
+})
+fs.writeFile('./output/names.json', JSON.stringify(names), err => {
+	if (err) throw err
+	fs.writeFile('./output/years.json', JSON.stringify(years), err => {
+		if (err) throw err
+		else console.timeEnd('populate data')
+	})
 })
 
